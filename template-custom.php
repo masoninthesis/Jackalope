f09ffd944109844525bec51e980efc21c685aedc
Mason L <mason.lawlor@me.com>
2014-09-24 15:58:50 -0700

    The Creation

---
+++ template-custom.php
@@ -0,0 +1,207 @@
+<?php
+/*
+Template Name: HDY Homepage
+*/
+?>
+
+<?php get_template_part('templates/page', 'header'); ?>
+
+<script type="text/javascript">
+	$('document').ready(function(){
+		$('#myToolTip1').tooltip();
+		$('#myToolTip2').tooltip();
+		$('#myToolTip3').tooltip();
+		$('#myToolTip4').tooltip();
+		$('#myToolTip5').tooltip();
+	});
+</script>
+
+<h1><i class="fa fa-info-circle vert-top" id="myToolTip1" data-toggle="tooltip" data-placement="top" title="If you want to plug into my mind, this is where you start"></i> About Me</h1>
+	<div class="row icons">
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="http://howdareyou.io/man-ifesto/" class="squircle"><i class="fa fa-paragraph"></i></a></div>
+			<div class="btn-text"><a href="http://howdareyou.io/man-ifesto/"><h5>Top Posts</h5></a></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="http://howdareyou.io/my-library/" class="squircle"><i class="fa fa-book"></i></a></div>
+			<div class="btn-text"><a href="http://howdareyou.io/my-library/"><h5>Library</h5></a></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="http://howdareyou.io/portfolio/" class="squircle"><i class="fa fa-th-large"></i></a></div>
+			<div class="btn-text"><a href="http://howdareyou.io/portfolio/"><h5>Portfolio</h5></a></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="http://howdareyou.io/category/i-dare-you/" class="squircle" id="myToolTip4" data-toggle="tooltip" data-placement="top" title="Subscribe to unlock"><i class="fa fa-lock"></i></a></div>
+			<div class="btn-text"><a href="http://howdareyou.io/category/i-dare-you/"><h5>Lifehacks</h5></a></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle" id="myToolTip5" data-toggle="tooltip" data-placement="top" title="Subscribe to unlock"><i class="fa fa-lock"></i></a></div>
+			<div class="btn-text"><h5>Plugins</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="http://howdareyou.io/about-me/" class="squircle"><i class="fa fa-ellipsis-h"></i></a></div>
+			<div class="btn-text"><a href="http://howdareyou.io/about-me/"><h5>More</h5></a></div>
+		</div>
+	</div>
+	
+<hr>
+
+<h1><i class="fa fa-info-circle vert-top" id="myToolTip2" data-toggle="tooltip" data-placement="top" title="This is the posting schedule I aim for each week"></i> Content Schedule</h1><br>
+	<div class="row icons content-sched">
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/hdy-icon.png" alt="HowDareYou Post"></a></div>
+			<div class="btn-text"><h5>Sunday</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/smartkit-icon.png" alt="SmartKit Post"></a></div>
+			<div class="btn-text"><h5>Monday</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/crowdfundation-icon.png" alt="CrowdFundation Post"></a></div>
+			<div class="btn-text"><h5>Tuesday</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/jackalope-icon.png" alt="Jackalope Post"></a></div>
+			<div class="btn-text"><h5>Wednesday</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/hdy-icon.png" alt="HowDareYou Lifehack or Guest Post"></a></div>
+			<div class="btn-text"><h5>Thursday</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/jackalope-icon.png" alt="Jackalope Tutorial"></a></div>
+			<div class="btn-text"><h5>Friday</h5></div> 
+		</div>
+	</div>
+	
+<hr>
+	
+<h1><i class="fa fa-info-circle vert-top" id="myToolTip3" data-toggle="tooltip" data-placement="top" title="These are the vital elements to my days"></i> Daily Routine</h1>
+	<div class="row icons">
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="http://howdareyou.io/daily-routine-bootup/" class="squircle"><i class="fa fa-power-off"></i></a></div>
+			<div class="btn-text"><a href="http://howdareyou.io/daily-routine-bootup/"><h5>Bootup</h5></a></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-bookmark"></i></a></div>
+			<div class="btn-text"><h5>Consume</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-spinner"></i></a></div>
+			<div class="btn-text"><h5>Create</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-lemon-o"></i></a></div>
+			<div class="btn-text"><h5>Cook</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-child"></i></a></div>
+			<div class="btn-text"><h5>Workout</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-power-off"></i></a></div>
+			<div class="btn-text"><h5>Shutdown</h5></div>
+		</div>
+	</div>
+	
+<hr>
+
+<h1><i class="fa fa-info-circle vert-top" style="color: #fff"></i> Follow Me</h1>
+	<div class="row icons">
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-envelope"></i></a></div>
+			<div class="btn-text"><h5>Email</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-twitter"></i></a></div>
+			<div class="btn-text"><h5>Twitter</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-facebook"></i></a></div>
+			<div class="btn-text"><h5>Facebook</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-instagram"></i></a></div>
+			<div class="btn-text"><h5>Instagram</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-pinterest"></i></a></div>
+			<div class="btn-text"><h5>Pinterest</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-google-plus-square"></i></a></div>
+			<div class="btn-text"><h5>Google+</h5></div>
+		</div>
+	</div>
+	<div class="row icons">
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-spotify"></i></a></div>
+			<div class="btn-text"><h5>Spotify</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-github"></i></a></div>
+			<div class="btn-text"><h5>Github</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-linkedin"></i></a></div>
+			<div class="btn-text"><h5>LinkedIn</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-reddit"></i></a></div>
+			<div class="btn-text"><h5>Reddit</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-behance"></i></a></div>
+			<div class="btn-text"><h5>Behance</h5></div>
+		</div>
+		<div class="col-md-2 col-sm-4 col-xs-6">
+			<div class="icon"><a href="#" class="squircle"><i class="fa fa-dribbble"></i></a></div>
+			<div class="btn-text"><h5>Dribbble</h5></div>
+		</div>
+	</div>
+
+<!-- pop-up modal -->
+<script type="text/javascript"
+        src="//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.3.1/jquery.cookie.min.js"></script>
+  <script type="text/javascript">
+    function showPopup() {
+      $('#myModal').modal('show');
+    }
+
+    function timeOut() {
+      if ($.cookie('foreverjobless') !== '1') {
+        setTimeout(function() {
+          showPopup();
+          jQuery.cookie('foreverjobless', '1', { expires: 7 });
+        }, 400000);
+      }
+    }
+
+    $(document).ready(function () {
+      timeOut();
+      stickyElement();
+    });
+
+  </script>
+
+   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+        	<center><h3>Get with the program.</h3></center>
+          	<div class="modal-body">
+          	<center><iframe src="//fast.wistia.net/embed/iframe/5tl43fy1m9?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="640" height="360"></iframe><script src="//fast.wistia.net/assets/external/iframe-api-v1.js"></script></center>
+    			
+	      
+					      <!-- Mailchimp -->
+							<center><form class="form-inline" id="before_header" action="//smartk.us8.list-manage.com/subscribe/post?u=99a776394eb3b3549e7c5e53c&amp;id=6a60137cbc" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+								<input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="Email address.." required>
+							<button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="modal-btn btn btn-lg" type="submit" value="Submit" tabindex="501">Subscribe</button>
+							
+							</form></center> <!-- /Mailchimp -->
+	      </div><!-- /optin -->
+		  <br><br>
+          </div>
+        </div>
+      </div>
+    </div>
+<!-- /pop-up modal -->
---
+++ templates/header-top-navbar.php
@@ -0,0 +1,55 @@
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/HDY.png" alt="<?php bloginfo('name'); ?>" /></a>
+    </div>
+
+    <nav class="collapse navbar-collapse" role="navigation">
+      <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+        endif;
+      ?>
+    </nav>
+  </div>
+</header>
+
+<section class="hero" id="hero">
+	<div class="wrap container">
+		<div class="row">
+			<div class="col-sm-12">
+ 			    <h1>Welcome to my mind.</h1>
+ 			    <h2>This is my external operating system.</h2>
+			    <a href="//fast.wistia.net/embed/iframe/5tl43fy1m9?popover=true" class="wistia-popover[height=360,playerColor=a7aab4,width=640]"><img src="https://embed-ssl.wistia.com/deliveries/b9dbc6afad01a1a2f8662ecf5054e48f17e65088.jpg?image_play_button=true&image_play_button_color=a7aab4e0&image_crop_resized=150x84" alt="" /></a>
+<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/popover-v1.js"></script>
+				<button href="#" type="button" type="button" class="btn btn-default hero-btn" data-toggle="modal" data-target="#myModal">Subscribe</button>
+			</div>
+	    </div>
+	  </div> <!-- /Video -->
+	    
+	 <!-- <div class="alert-header col-sm-10 col-sm-offset-1"><!-- Optin --
+	      <div class="row">
+	        <h4 class="optin-h4">11 powerful lifehacks to your inbox, one day at a time.</h4>
+	      </div>
+		  <div class="row">
+		      <div class="top-optin col-sm-offset-4">
+		      
+		      <!-- Mailchimp --
+				<form class="form-inline" id="before_header" action="//howdareyou.us8.list-manage.com/subscribe/post?u=4c05f20503bc3a9d84d1a63c9&amp;id=488afa7633" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="Email address.." required>
+				<button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="submit btn btn-primary btn-lg" type="submit" value="Submit" tabindex="501">I Dare</button>
+				
+				</form> <!-- /Mailchimp -->
+		      </div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+</section>
+
