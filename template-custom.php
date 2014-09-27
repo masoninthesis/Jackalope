33d746b4059a20e0f09037de93ee282d45903b91
Mason L <mason.lawlor@me.com>
2014-09-27 14:44:45 -0700

    home content responsive optimization

--- template-custom.php
+++ template-custom.php
@@ -7,14 +7,14 @@ Template Name: HDY Homepage
 <?php get_template_part('templates/page', 'header'); ?>
 
 		<div class="row home-section">
-			<div class="col-sm-6">
+			<div class="col-sm-6 col-sm-push-6">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/home/graph.png" alt="graph" width="468" height="212" />
+			</div>
+			<div class="col-sm-6 col-sm-pull-6">
 				<h4>Growth Hacking.</h4>
-				<p>"What is a growth hacking? It’s simple, we use unconventional methods to make you more money. hether you’re looking to bring more traffic to your website, make more of that traffic convert, or both– we can help you with that. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor."</p>
+				<p>"What is a growth hacking? It’s simple, we use unconventional methods to make you more money. Whether you’re looking to bring more traffic to your website, make more of that traffic convert, or both– we can help you with that. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor."</p>
 				<p><a href="#">link goes here</a></p>
 				<button class="btn btn-defualt btn-md">View Pricing</button>
-			</div>
-			<div class="col-sm-6">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/home/graph.png" alt="graph" width="468" height="212" />
 			</div>
 		</div>
 		<div class="row home-section">
@@ -23,7 +23,7 @@ Template Name: HDY Homepage
 			</div>
 			<div class="col-sm-6">
 				<h4>Websites.</h4>
-				<p>"Jackalope is an open source company, which means that all the web templates we’ve built are available to you for free. The trick is to know how to install them, we can help you with that too in our tutoiral videos. If it’s still too hard, you can pay us to do it. How awesome is that?"</p>
+				<p>"Jackalope is an open source company, which means that all the web templates we’ve built are available to you for free. The trick is to know how to install them, we can help you with that too in our tutorial videos. If it’s still too hard, you can pay us to do it. How awesome is that?"</p>
 				<p><a href="#">Tutorials</a></p>
 				<button class="btn btn-defualt btn-md">Browse Themes</button>
 			</div>
@@ -33,13 +33,13 @@ Template Name: HDY Homepage
 				<h4>Videos.</h4>
 				<p>"What is a growth hacking? It’s simple, we use unconventional methods to make you more money. hether you’re looking to bring more traffic to your website, make more of that traffic convert, or both– we can help you with that. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor."</p>
 				<div class="row">
-					<div class="col-sm-4">
+					<div class="col-xs-4">
 						<center><a href="//fast.wistia.net/embed/iframe/5tl43fy1m9?popover=true" class="wistia-popover[height=360,playerColor=a7aab4,width=640]"><img src="https://embed-ssl.wistia.com/deliveries/b9dbc6afad01a1a2f8662ecf5054e48f17e65088.jpg?image_play_button=true&image_play_button_color=a7aab4e0&image_crop_resized=150x84" alt="" /></a></center>
 					</div>
-					<div class="col-sm-4">
+					<div class="col-xs-4">
 						<center><a href="//fast.wistia.net/embed/iframe/5tl43fy1m9?popover=true" class="wistia-popover[height=360,playerColor=a7aab4,width=640]"><img src="https://embed-ssl.wistia.com/deliveries/b9dbc6afad01a1a2f8662ecf5054e48f17e65088.jpg?image_play_button=true&image_play_button_color=a7aab4e0&image_crop_resized=150x84" alt="" /></a></center>
 					</div>
-					<div class="col-sm-4">
+					<div class="col-xs-4">
 						<center><a href="//fast.wistia.net/embed/iframe/5tl43fy1m9?popover=true" class="wistia-popover[height=360,playerColor=a7aab4,width=640]"><img src="https://embed-ssl.wistia.com/deliveries/b9dbc6afad01a1a2f8662ecf5054e48f17e65088.jpg?image_play_button=true&image_play_button_color=a7aab4e0&image_crop_resized=150x84" alt="" /></a></center>
 					</div>
 				</div>
