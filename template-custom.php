35168d71b9560c6638e9d5069f94e12590b3842c
Mason L <mason.lawlor@me.com>
2014-09-27 02:03:35 -0700

    Added code to home-template: missing imgs

--- template-custom.php
+++ template-custom.php
@@ -6,159 +6,46 @@ Template Name: HDY Homepage
 
 <?php get_template_part('templates/page', 'header'); ?>
 
-<script type="text/javascript">
-	$('document').ready(function(){
-		$('#myToolTip1').tooltip();
-		$('#myToolTip2').tooltip();
-		$('#myToolTip3').tooltip();
-		$('#myToolTip4').tooltip();
-		$('#myToolTip5').tooltip();
-	});
-</script>
-
-<h1><i class="fa fa-info-circle vert-top" id="myToolTip1" data-toggle="tooltip" data-placement="top" title="If you want to plug into my mind, this is where you start"></i> About Me</h1>
-	<div class="row icons">
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="http://howdareyou.io/man-ifesto/" class="squircle"><i class="fa fa-paragraph"></i></a></div>
-			<div class="btn-text"><a href="http://howdareyou.io/man-ifesto/"><h5>Top Posts</h5></a></div>
+		<div class="row home-section">
+			<div class="col-sm-6">
+				<h4>Growth Hacking.</h4>
+				<p>"What is a growth hacking? It’s simple, we use unconventional methods to make you more money. hether you’re looking to bring more traffic to your website, make more of that traffic convert, or both– we can help you with that. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor."</p>
+				<p><a href="#">link goes here</a></p>
+				<button class="btn btn-defualt btn-md">View Pricing</button>
+			</div>
+			<div class="col-sm-6">
+				<img src="...">
+			</div>
 		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="http://howdareyou.io/my-library/" class="squircle"><i class="fa fa-book"></i></a></div>
-			<div class="btn-text"><a href="http://howdareyou.io/my-library/"><h5>Library</h5></a></div>
+		<div class="row home-section">
+			<div class="col-sm-6">
+				<img src="...">
+			</div>
+			<div class="col-sm-6">
+				<h4>Websites.</h4>
+				<p>"Jackalope is an open source company, which means that all the web templates we’ve built are available to you for free. The trick is to know how to install them, we can help you with that too in our tutoiral videos. If it’s still too hard, you can pay us to do it. How awesome is that?"</p>
+				<p><a href="#">Tutorials</a></p>
+				<button class="btn btn-defualt btn-md">Browse Themes</button>
+			</div>
 		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="http://howdareyou.io/portfolio/" class="squircle"><i class="fa fa-th-large"></i></a></div>
-			<div class="btn-text"><a href="http://howdareyou.io/portfolio/"><h5>Portfolio</h5></a></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="http://howdareyou.io/category/i-dare-you/" class="squircle" id="myToolTip4" data-toggle="tooltip" data-placement="top" title="Subscribe to unlock"><i class="fa fa-lock"></i></a></div>
-			<div class="btn-text"><a href="http://howdareyou.io/category/i-dare-you/"><h5>Lifehacks</h5></a></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle" id="myToolTip5" data-toggle="tooltip" data-placement="top" title="Subscribe to unlock"><i class="fa fa-lock"></i></a></div>
-			<div class="btn-text"><h5>Plugins</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="http://howdareyou.io/about-me/" class="squircle"><i class="fa fa-ellipsis-h"></i></a></div>
-			<div class="btn-text"><a href="http://howdareyou.io/about-me/"><h5>More</h5></a></div>
-		</div>
-	</div>
-	
-<hr>
-
-<h1><i class="fa fa-info-circle vert-top" id="myToolTip2" data-toggle="tooltip" data-placement="top" title="This is the posting schedule I aim for each week"></i> Content Schedule</h1><br>
-	<div class="row icons content-sched">
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/hdy-icon.png" alt="HowDareYou Post"></a></div>
-			<div class="btn-text"><h5>Sunday</h5></div> 
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/smartkit-icon.png" alt="SmartKit Post"></a></div>
-			<div class="btn-text"><h5>Monday</h5></div> 
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/crowdfundation-icon.png" alt="CrowdFundation Post"></a></div>
-			<div class="btn-text"><h5>Tuesday</h5></div> 
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/jackalope-icon.png" alt="Jackalope Post"></a></div>
-			<div class="btn-text"><h5>Wednesday</h5></div> 
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/hdy-icon.png" alt="HowDareYou Lifehack or Guest Post"></a></div>
-			<div class="btn-text"><h5>Thursday</h5></div> 
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/jackalope-icon.png" alt="Jackalope Tutorial"></a></div>
-			<div class="btn-text"><h5>Friday</h5></div> 
-		</div>
-	</div>
-	
-<hr>
-	
-<h1><i class="fa fa-info-circle vert-top" id="myToolTip3" data-toggle="tooltip" data-placement="top" title="These are the vital elements to my days"></i> Daily Routine</h1>
-	<div class="row icons">
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="http://howdareyou.io/daily-routine-bootup/" class="squircle"><i class="fa fa-power-off"></i></a></div>
-			<div class="btn-text"><a href="http://howdareyou.io/daily-routine-bootup/"><h5>Bootup</h5></a></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-bookmark"></i></a></div>
-			<div class="btn-text"><h5>Consume</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-spinner"></i></a></div>
-			<div class="btn-text"><h5>Create</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-lemon-o"></i></a></div>
-			<div class="btn-text"><h5>Cook</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-child"></i></a></div>
-			<div class="btn-text"><h5>Workout</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-power-off"></i></a></div>
-			<div class="btn-text"><h5>Shutdown</h5></div>
-		</div>
-	</div>
-	
-<hr>
-
-<h1><i class="fa fa-info-circle vert-top" style="color: #fff"></i> Follow Me</h1>
-	<div class="row icons">
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-envelope"></i></a></div>
-			<div class="btn-text"><h5>Email</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-twitter"></i></a></div>
-			<div class="btn-text"><h5>Twitter</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-facebook"></i></a></div>
-			<div class="btn-text"><h5>Facebook</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-instagram"></i></a></div>
-			<div class="btn-text"><h5>Instagram</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-pinterest"></i></a></div>
-			<div class="btn-text"><h5>Pinterest</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-google-plus-square"></i></a></div>
-			<div class="btn-text"><h5>Google+</h5></div>
-		</div>
-	</div>
-	<div class="row icons">
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-spotify"></i></a></div>
-			<div class="btn-text"><h5>Spotify</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-github"></i></a></div>
-			<div class="btn-text"><h5>Github</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-linkedin"></i></a></div>
-			<div class="btn-text"><h5>LinkedIn</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-reddit"></i></a></div>
-			<div class="btn-text"><h5>Reddit</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-behance"></i></a></div>
-			<div class="btn-text"><h5>Behance</h5></div>
-		</div>
-		<div class="col-md-2 col-sm-4 col-xs-6">
-			<div class="icon"><a href="#" class="squircle"><i class="fa fa-dribbble"></i></a></div>
-			<div class="btn-text"><h5>Dribbble</h5></div>
-		</div>
-	</div>
+		<div class="row home-section">
+			<div class="col-sm-12">
+				<h4>Videos.</h4>
+				<p>"What is a growth hacking? It’s simple, we use unconventional methods to make you more money. hether you’re looking to bring more traffic to your website, make more of that traffic convert, or both– we can help you with that. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor."</p>
+				<div class="row">
+					<div class="col-sm-4">
+						<center><img src="..."></center>
+					</div>
+					<div class="col-sm-4">
+						<center><img src="..."></center>
+					</div>
+					<div class="col-sm-4">
+						<center><img src="..."></center>
+					</div>
+				</div>
+				<button class="btn btn-defualt btn-md">Get quote</button>
+			</div>
+		</div> 
 
 <!-- pop-up modal -->
 <script type="text/javascript"
